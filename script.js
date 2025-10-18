36ca9609d6f492968b16b80f981b6f24ff6def70
Taofeek Kassim <taophyckassim@gmail.com>
2025-10-18 13:53:00 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Update time in milliseconds
+const timeEl = document.querySelector('[data-testid="test-user-time"]');
+
+function updateTime() {
+  timeEl.textContent = Date.now();
+}
+
+updateTime();
+
+setInterval(updateTime, 1000);
